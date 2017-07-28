9e6c705e31d867bcf1e955461169f2ccc86af09d
Terry Kwon <terrykkwon@gmail.com>
2017-07-28 19:47:45 +0900

    Solve 'Reverse Array'

---
+++ src/com/terrykwon/ReverseArray.java
@@ -0,0 +1,31 @@
+package com.terrykwon;
+
+/**
+ * Reverse an array in-place.
+ */
+public class ReverseArray {
+
+    /**
+     * Swaps the first and last character, and continues until the halfway point.
+     *
+     * Time complexity: O(N)
+     * Space complexity: O(1)
+     */
+    private static void reverseArray(char[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "hello this is a potato";
+        char[] arr = s.toCharArray();
+
+        reverseArray(arr);
+
+        System.out.println(new String(arr));
+
+    }
+}
