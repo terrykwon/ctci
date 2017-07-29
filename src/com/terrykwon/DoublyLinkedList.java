a7c116edc1f610b6e2ae03f123f570cf2b958382
Terry Kwon <terrykkwon@gmail.com>
2017-07-30 00:31:55 +0900

    Implement doubly linked list

---
+++ src/com/terrykwon/DoublyLinkedList.java
@@ -0,0 +1,140 @@
+package com.terrykwon;
+
+/**
+ * An implementation of a doubly linked list.
+ *
+ * Uses sentinel header and trailer nodes for a cleaner implementation.
+ * The sentinels ensure that node.getPrev() and node.getNext() never return null; they instead return a
+ * node with null values.
+ */
+public class DoublyLinkedList<E> {
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public DoublyLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return header.getNext().getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return trailer.getPrev().getElement(); // Can't you return trailer.element since null anyways?
+    }
+
+    private void addBetween(E element, Node<E> predecessor, Node<E> successor) {
+        Node<E> node = new Node<>(element, predecessor, successor);
+        predecessor.setNext(node);
+        successor.setPrev(node);
+        size++;
+    }
+
+    public void addFirst(E element) {
+        addBetween(element, header, header.getNext());
+    }
+
+    public void addLast(E element) {
+        addBetween(element, trailer.getPrev(), trailer);
+    }
+
+    private E remove(Node<E> node) {
+        Node<E> predecessor = node.getPrev();
+        Node<E> successor = node.getNext();
+
+        predecessor.setNext(successor);
+        successor.setPrev(predecessor);
+
+        size --;
+
+        return node.getElement();
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return remove(header.getNext());
+    }
+
+    public E removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return remove(trailer.getPrev());
+    }
+
+    /**
+     * Converts the linked list to a string.
+     * The element type must either be primitive or have an overridden {@code toString()} method.
+     *
+     * @return  The string representation of the linked list.
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node node = header.getNext();
+
+        while (node != trailer) {
+            builder.append(node.getElement());
+            node = node.getNext();
+        }
+
+        return builder.toString();
+    }
+
+
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E element, Node<E> prev, Node<E> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+}
---
+++ src/com/terrykwon/DoublyLinkedListTest.java
@@ -0,0 +1,83 @@
+package com.terrykwon;
+
+import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+
+
+public class DoublyLinkedListTest {
+
+//    public static void main(String[] args) {
+//        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+//        System.out.println(list);
+//
+//        list.addFirst(1);
+//        System.out.println(list);
+//
+//        list.addFirst(0);
+//        list.addLast(2);
+//        System.out.println(list);
+//
+//        System.out.println(list.first());
+//        System.out.println(list.last());
+//    }
+
+    @Test
+    public void testToString() throws Exception {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertEquals("", list.toString());
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+
+        assertEquals("1234", list.toString());
+    }
+
+    @Test
+    public void testRemoveFirst() throws Exception {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertNull(list.removeFirst());
+
+        list.addFirst(1);
+        list.addFirst(2);
+
+        assertEquals(2, (int) list.removeFirst());
+        assertEquals(1, (int) list.removeFirst());
+    }
+
+    @Test
+    public void testRemoveLast() throws Exception {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertNull(list.removeLast());
+
+        list.addLast(1);
+        list.addLast(2);
+
+        assertEquals(2, (int) list.removeLast());
+        assertEquals(1, (int) list.removeLast());
+    }
+
+    @Test
+    public void testFirst() throws Exception {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertNull(list.first());
+
+        list.addFirst(1);
+        list.addFirst(2);
+
+        assertEquals(2, (int) list.first());
+    }
+
+    @Test
+    public void testLast() throws Exception {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertNull(list.last());
+
+        list.addFirst(1);
+        list.addFirst(2);
+
+        assertEquals(1, (int) list.last());
+    }
+
+}
