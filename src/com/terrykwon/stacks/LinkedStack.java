f98c39ad0cd6f172bc735ce5aaf434a1e287b9c3
Terry Kwon <terrykkwon@gmail.com>
2017-08-15 21:15:41 +0900

    Implement LinkedStack class

---
+++ src/com/terrykwon/stacks/LinkedStack.java
@@ -0,0 +1,35 @@
+package com.terrykwon.stacks;
+
+import com.terrykwon.linkedlists.SinglyLinkedList;
+
+/**
+ * An implementation of a LIFO stack using a singly linked list.
+ */
+public class LinkedStack<E> implements Stack<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E element) {
+        list.addFirst(element);
+    }
+
+    @Override
+    public E top() {
+        return list.first();
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+}
