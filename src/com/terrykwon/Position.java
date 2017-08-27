9b1a0e99fdc47f725a59c854388600f5107c1f25
Terry Kwon <terrykkwon@gmail.com>
2017-08-27 15:54:27 +0900

    Add position interface

---
+++ src/com/terrykwon/Position.java
@@ -0,0 +1,16 @@
+package com.terrykwon;
+
+/**
+ * An interface for a positional data structure.
+ */
+public interface Position<E> {
+
+    /**
+     * Returns the element stored at this position.
+     *
+     * @return  the stored element.
+     * @throws IllegalStateException    if the position is no longer valid.
+     */
+    E getElement() throws IllegalStateException;
+
+}
