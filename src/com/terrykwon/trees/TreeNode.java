59036ebfbe22988e83dd57a6be585999344e75a9
Terry Kwon <terrykkwon@gmail.com>
2017-09-07 09:40:15 +0900

    Add TreeNode class for standalone tree problems

---
+++ src/com/terrykwon/trees/TreeNode.java
@@ -0,0 +1,21 @@
+package com.terrykwon.trees;
+
+/**
+ * A node class for use in CTCI problems.
+ */
+public class TreeNode<E> {
+    E element;
+    TreeNode<E> leftChild;
+    TreeNode<E> rightChild;
+
+    public TreeNode(E element) {
+        this(element, null, null);
+    }
+
+    public TreeNode(E element, TreeNode<E> leftChild, TreeNode<E> rightChild) {
+        this.element = element;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+}
