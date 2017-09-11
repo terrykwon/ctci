c6026ae6c63f64103e2cdc6f18395981bf3a3750
Terry Kwon <terrykkwon@gmail.com>
2017-09-11 21:18:51 +0900

    Add time and space complexity to 'MinimalTree'

--- src/com/terrykwon/trees/MinimalTree.java
+++ src/com/terrykwon/trees/MinimalTree.java
@@ -1,7 +1,7 @@
 package com.terrykwon.trees;
 
-import static com.terrykwon.Utils.printInorder;
-import static com.terrykwon.Utils.printRotated;
+import static com.terrykwon.Utils.printTreeInorder;
+import static com.terrykwon.Utils.printTreeRotated;
 
 /**
  * Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a
@@ -17,6 +17,9 @@ public class MinimalTree {
      * Uses a recursive approach where the middle element of the array is inserted as the root,
      * while the middle element of the left subarray becomes the left child, and the middle element of the right
      * subarray becomes the right child.
+     *
+     * Time complexity: O(N)
+     * Space complexity: O(N), due to recursion.
      *
      * @param arr an Integer array sorted in increasing order, with no duplicate elements.
      * @return the root of the generated binary search tree.
@@ -53,21 +56,21 @@ public class MinimalTree {
         Integer[] a2 = {1, 2, 3, 4, 5, 6, 7, 8};
         TreeNode n2 = treeFromArray(a2);
 
-        printInorder(n1);
+        printTreeInorder(n1);
         System.out.println();
-        printRotated(n1);
+        printTreeRotated(n1);
 
         System.out.println();
 
-        printInorder(n2);
+        printTreeInorder(n2);
         System.out.println();
-        printRotated(n2);
+        printTreeRotated(n2);
 
         Integer[] a3 = {99};
         TreeNode n3 = treeFromArray(a3);
 
         System.out.println();
-        printInorder(n3);
+        printTreeInorder(n3);
 
     }
 }
