2eb0fe212135bb8a47d33019fd6b2c40cd2694a4
Terry Kwon <terrykkwon@gmail.com>
2017-09-23 11:21:50 +0900

    Solve 'BST Sequences'

--- src/com/terrykwon/trees/BSTSequences.java
+++ src/com/terrykwon/trees/BSTSequences.java
@@ -3,7 +3,6 @@ package com.terrykwon.trees;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * A BST was created by traversing through an array from left to right and inserting each element. Given a BST
@@ -13,6 +12,46 @@ public class BSTSequences {
 
     private BSTSequences() {
 
+    }
+
+    /**
+     * A solution using two recursive algorithms. Assuming that we have every viable sequence for the left
+     * and right subtree of a node, weaving them together (and prepending the root's element) will yield
+     * all viable sequences of the current node. This is repeated for all subtrees, recursing from the leaves.
+     *
+     * Time complexity: O(2^N)
+     * Space complexity: O(2^N), b/c the number of permutations increase exponentially.
+     */
+    private static <E> ArrayList<LinkedList<E>> sequences(TreeNode<E> root) {
+        if (root == null) {
+            // Return an ArrayList with one empty LinkedList
+            // The empty LinkedList is necessary b/c if the ArrayList is empty, the for loop below won't be called,
+            // and a list containing just the prefix (root element) won't be returned.
+            ArrayList<LinkedList<E>> empty = new ArrayList<>();
+            empty.add(new LinkedList<>());
+            return empty;
+        }
+
+        ArrayList<LinkedList<E>> results = new ArrayList<>();
+
+        // Pass a "ready-made" prefix with the current root element to weaveLists().
+        LinkedList<E> prefix = new LinkedList<>();
+        prefix.add(root.element);
+
+        // Assume we have every viable sequence for the left and right subtrees.
+        ArrayList<LinkedList<E>> leftSeq = sequences(root.leftChild);
+        ArrayList<LinkedList<E>> rightSeq = sequences(root.rightChild);
+
+        // All possible weaves (permutations) of two viable sequences will be a valid sequence for the current root.
+        for (LinkedList<E> left : leftSeq) {
+            for (LinkedList<E> right : rightSeq) {
+                ArrayList<LinkedList<E>> weaved = new ArrayList<>();
+                weaveLists(left, right, prefix, weaved);
+                results.addAll(weaved);
+            }
+        }
+
+        return results;
     }
 
     /**
@@ -55,8 +94,21 @@ public class BSTSequences {
 
         weaveLists(l1, l2, new LinkedList<>(), w1);
 
-        printListOfLists(w1);
+//        printListOfLists(w1);
 
+        TreeNode<Integer> t1 = buildTree();
+        ArrayList<LinkedList<Integer>> r1 = sequences(t1);
+
+        printListOfLists(r1);
+
+    }
+
+    private static TreeNode<Integer> buildTree() {
+        TreeNode<Integer> root = new TreeNode<>(2);
+        root.leftChild = new TreeNode<>(1);
+        root.rightChild = new TreeNode<>(3);
+
+        return root;
     }
 
     private static <E, L extends Iterable<E>> void printListOfLists(Iterable<L> list) {
